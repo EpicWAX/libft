1b316dc17a8bed22af32cf4f15caaff6077c72ec
Quentin HUSLER <qhusler@dump-mini-3.42.fr>
2016-01-22 20:32:07 +0100

    .

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: qhusler <qhusler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 18:41:46 by qhusler           #+#    #+#             */
-/*   Updated: 2016/01/17 00:41:10 by qhusler          ###   ########.fr       */
+/*   Updated: 2016/01/22 20:30:44 by qhusler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,22 @@
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	t_list *newlst;
+	t_list	*newlst;
+	void	*c;
 
+	if (!(c = (void*)ft_memalloc(content_size)))
+		return (NULL);
+	if (content == NULL)
+	{
+		c = NULL;
+		content_size = 0;
+	}
+	else
+		c = ft_memcpy(c, content, content_size);
 	if (!(newlst = (t_list*)malloc(sizeof(newlst) * content_size)))
 		return (NULL);
-	if (!(content))
-	{
-		newlst->content = NULL;
-		newlst->content_size = 0;
-		return (newlst);
-	}
-	newlst->content = malloc(content_size);
-	newlst->content = ft_memcpy(newlst->content, content, content_size);
+	newlst->content = c;
+	newlst->content_size = content_size;
 	newlst->next = NULL;
 	return (newlst);
 }
